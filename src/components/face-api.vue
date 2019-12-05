894008cc1ce280327ca8a16c11def2e8cd0ed569
superluyun <1437505934@qq.com>
2019-12-05 17:51:08 +0800

    12.5

---
+++ src/components/face-api.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import * as faceapi from 'face-api.js'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      
+    }
+  },
+  mounted(){
+    console.log(faceapi.nets)
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
